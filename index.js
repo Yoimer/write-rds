3db3cde93da7e5b4270be655e6ba52968900c45c
Yoimer Román <ydrf19@gmail.com>
2020-08-14 16:16:04 -0400

    saves on mysql although AWS shows timeout error

--- index.js
+++ index.js
@@ -12,78 +12,49 @@ const docClient = new AWS.DynamoDB.DocumentClient({
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
+    //var registers = "INSERT INTO SENSORS (id , timestamp, temperature, humidity, pressure ,lpg ,co2 ,latitude ,longitude ,siteid ,site ,location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
 
-    //nuevos cambios
-    console.log('datos de entrada fuera de la funcion');
-    console.log('id:', event.id);
-    console.log('temperature:', event.temperature);
-    console.log('humidity:', event.humidity);
-    console.log('pressure:', event.pressure);
-    console.log('lpg:', event.lpg);
-    console.log('co2:', event.co2);
-    console.log('latitude:', event.latitude);
-    console.log('longitude:', event.longitude);
-    console.log('siteid:', event.siteid);
-    console.log('site:', event.site);
-    console.log('location:', event.location);
-    // fin nuevos cambios
-    //read_location(event.id);
+    const pool = mysql.createConnection({
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT,
+        database: process.env.RDS_DATABASE
+    });
 
-    function data_write(mydata) {
 
-        const pool = mysql.createPool({
-            host: process.env.RDS_HOSTNAME,
-            user: process.env.RDS_USERNAME,
-            password: process.env.RDS_PASSWORD,
-            port: process.env.RDS_PORT,
-            database: process.env.RDS_DATABASE
+    console.log('datos dentro de la funcion', event);
+    var idd = JSON.stringify(event.id);
+    var timestamp = Date.now();
+    var temperature = event.temperature;
+    var humidity = event.humidity;
+    var pressure = event.pressure;
+    var lpg = event.lpg;
+    var co2 = event.co2
+    var latitude = event.latitude;
+    var longitude = event.longitude;
+    var siteid = event.siteid;
+    var site = event.site;
+    var location = event.location;
+
+    context.callbackWaitsForEmptyEventLoop = false;
+
+
+    //var records = [idd, timestamp, temperature, humidity, pressure, lpg, co2, latitude, longitude, siteid, site, location];
+    
+    //console.log("records", records);
+    
+    // make to connection to the database.
+    pool.connect(function(err) {
+        if (err) throw err;
+        // if connection is successful
+        var records = [[idd, timestamp, temperature, humidity, pressure, lpg, co2, latitude, longitude, siteid, site, location]];
+        pool.query("INSERT INTO SENSORS (id , timestamp, temperature, humidity, pressure ,lpg ,co2 ,latitude ,longitude ,siteid ,site ,location) VALUES ?", [records], function(err, result, fields) {
+            // if any error while executing above query, throw error
+            if (err) throw err;
+            // if there is no error, you have the result
+            console.log(result);
         });
+    });
 
-        //variables de la base de datos 
-        console.log('datos de la base de datos');
-        console.log('host:', process.env.RDS_HOSTNAME);
-        console.log('host:', process.env.RDS_USERNAME);
-        console.log('host:', process.env.RDS_PASSWORD);
-        console.log('host:', process.env.RDS_PORT);
-        console.log('host:', process.env.RDS_DATABASE);
-
-        console.log('datos dentro de la funcion', mydata);
-        var idd = JSON.stringify(event.id);
-        var timestamp = Date.now();
-        var latitude = mydata.latitude;
-        var longitude = mydata.longitude;
-        var siteid = mydata.site_id;
-        var site = mydata.site;
-        var location = mydata.address;
-
-        // nuevos cambios
-        var siteid = mydata.siteid;
-        var location = mydata.location;
-        var temperature = mydata.temperature;
-        var humidity = mydata.humidity;
-        var pressure = mydata.pressure;
-        var co2 = mydata.co2;
-        var lpg = mydata.lpg;
-        //fin nuevos cambios
-
-
-
-
-        console.log('variables procesada en la funcion');
-
-        //variables procesadas por la funcion 
-        console.log('id:', idd);
-        console.log('timestamp:', timestamp);
-        console.log('humidity:', event.humidity);
-        console.log('pressure:', event.pressure);
-        console.log('lpg:', event.lpg);
-        console.log('co2:', event.co2);
-        console.log('latitude:', latitude);
-        console.log('longitude:', longitude);
-        console.log('siteid:', siteid);
-        console.log('site:', event.site);
-        console.log('location:', location);
-
-    console.log("Hola soy lambda");
-    data_write(event);
-};
+}
